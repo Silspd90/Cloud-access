7872fe6fff92d0c1c3fb665e0eef648ee65ad17c
Silspd90 <86676267+Silspd90@users.noreply.github.com>
2022-01-24 11:19:12 +0530

    Added JS

---
+++ js/script.js
@@ -0,0 +1,93 @@
+function scrollbtn(clickedbtn){
+    if (clickedbtn == 'scroll_left'){
+        document.getElementById('groupscroller').scrollLeft -= 311;
+    }
+    else{
+        document.getElementById('groupscroller').scrollLeft += 311;        
+    }
+}
+
+function filtertable(){
+    let newinput = document.getElementById("inputtext");
+    let filter = newinput.value.toUpperCase();
+
+    let tablevalue = document.getElementById("group_table");
+    let rows = tablevalue.getElementsByTagName("tr");
+    let td,txtValue;
+
+
+    for (i = 1; i < rows.length; i++)
+    {
+        td = rows[i].getElementsByTagName("td")[1];
+        
+        txtValue = td.textContent;
+        if (txtValue.toUpperCase().indexOf(filter) > -1)
+        {   
+            rows[i].style.display = "table";
+        }
+        else{            
+            rows[i].style.display = "none";
+        }        
+    }
+}
+
+function debounce(func, timeout = 500){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+  }
+  const processChange = debounce(() => filtertable());
+  
+//----------------------------------------------------------------------
+
+
+  function sortTableByGroupName(table, column, asc = true) {
+    
+    const dirModifier = asc ? 1 : -1;
+    
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+    
+  
+  
+    const sortedRows = rows.sort((a, b) => {
+      const aColText = a.querySelectorAll("td")[1].textContent.trim();
+      const bColText = b.querySelectorAll("td")[1].textContent.trim();
+      return aColText > bColText ? 1 * dirModifier : -1 * dirModifier;
+    });
+  
+    
+    while (tBody.firstChild) {
+      tBody.removeChild(tBody.firstChild);
+    }
+  
+    
+    tBody.append(...sortedRows);
+  
+    table
+    .querySelectorAll("th")
+    .forEach((th) => th.classList.remove("th-sort-asc", "th-sort-desc"));
+  
+  table.querySelector("th").classList.toggle("th-sort-asc", asc);
+  table.querySelector("th").classList.toggle("th-sort-desc", !asc);
+  }
+  
+  
+  
+  
+  let whichSort = document.getElementById("sorting");
+  
+  whichSort.addEventListener("click", function () {
+  
+    
+    let tableElement = document.getElementById("group_table");
+  
+    let checkOrder = tableElement.getElementsByTagName("th")[0];
+    let currentIsAscending = checkOrder.classList.contains("th-sort-asc");
+    sortTableByGroupName(tableElement, 1, !currentIsAscending);
+  
+  });
+
+  
